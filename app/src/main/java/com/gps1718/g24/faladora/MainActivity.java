1cb9a8bfb042c3d39a78b9be56049e4ae934eb74
TotyDados <34349644+TotyDados@users.noreply.github.com>
2017-12-11 19:36:06 +0000

    v2.3
    Limpeza de Codigo. Comentario. Padronização.

--- app/src/main/java/com/gps1718/g24/faladora/MainActivity.java
+++ app/src/main/java/com/gps1718/g24/faladora/MainActivity.java
@@ -1,42 +1,38 @@
 package com.gps1718.g24.faladora;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    //lastOperation grava a operação a realizar
     String lastOperation = "";
+    //firstOperation diz se é o primeira operação da conta (não é possivel haver mais que uma)
     boolean firstOperation = true;
-    double soma = 0, valor1 = 0, valor2 = 0;
+    //variavel que coleta as informações do ecrã/resultado da conta
+    double soma = 0;
 
-    Button btt1, btt2, btt3, btt4, btt5, btt6, btt7, btt8, btt9, btt0, bttMais, bttMenos, bttMult, bttDiv, bttC, bttCE, bttEq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Por a calculadora em fullscreen
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-
+        //Definição de todos os botões click e long press
         //Butão 1
-        btt1 = (Button) findViewById(R.id.butaoUm);
-        btt1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //onSound(v);
-            }
-        });
-
-        btt1.setOnLongClickListener(new View.OnLongClickListener() {
-            public boolean onLongClick(View v) {
-                onNumero(v);
-                return true;
-            }
-        });
-
-        //Butão 1
-        btt1 = (Button) findViewById(R.id.butaoUm);
+        Button btt1;
+        btt1 = findViewById(R.id.butaoUm);
         btt1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //onSound(v);
@@ -51,6 +47,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 2
+        Button btt2;
         btt2 = (Button) findViewById(R.id.butaoDois);
         btt2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -66,6 +63,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 3
+        Button btt3;
         btt3 = (Button) findViewById(R.id.butaoTres);
         btt3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -81,6 +79,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 4
+        Button btt4;
         btt4 = (Button) findViewById(R.id.butaoQuatro);
         btt4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -96,6 +95,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 5
+        Button btt5;
         btt5 = (Button) findViewById(R.id.butaoCinco);
         btt5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -111,6 +111,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 6
+        Button btt6;
         btt6 = (Button) findViewById(R.id.butaoSeis);
         btt6.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -126,6 +127,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 7
+        Button btt7;
         btt7 = (Button) findViewById(R.id.butaoSete);
         btt7.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -141,6 +143,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 8
+        Button btt8;
         btt8 = (Button) findViewById(R.id.butaoOito);
         btt8.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -156,6 +159,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 9
+        Button btt9;
         btt9 = (Button) findViewById(R.id.butaoNove);
         btt9.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -171,6 +175,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão 0
+        Button btt0;
         btt0 = (Button) findViewById(R.id.butaoZero);
         btt0.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -186,6 +191,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão +
+        Button bttMais;
         bttMais = (Button) findViewById(R.id.butaoMais);
         bttMais.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -201,6 +207,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão -
+        Button bttMenos;
         bttMenos = (Button) findViewById(R.id.butaoMenos);
         bttMenos.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -216,6 +223,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão X
+        Button bttMult;
         bttMult = (Button) findViewById(R.id.butaoVezes);
         bttMult.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -231,6 +239,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão /
+        Button bttDiv;
         bttDiv = (Button) findViewById(R.id.butaoDiv);
         bttDiv.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -246,6 +255,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão C
+        Button bttC;
         bttC = (Button) findViewById(R.id.butaoApagar);
         bttC.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -261,6 +271,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão CE
+        Button bttCE;
         bttCE = (Button) findViewById(R.id.butaoApagTudo);
         bttCE.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -276,6 +287,7 @@ public class MainActivity extends Activity {
         });
 
         //Butão =
+        Button bttEq;
         bttEq = (Button) findViewById(R.id.butaoIgual);
         bttEq.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -290,20 +302,40 @@ public class MainActivity extends Activity {
             }
         });
 
+        //Butão Tutorial
+        Button bttTut;
+        bttTut = (Button) findViewById(R.id.butaoTut);
+        bttTut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //onSound(v);
+            }
+        });
+
+        bttTut.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+
+                DialogFragment newFragment = new onTut();
+                newFragment.show(getFragmentManager(), "Missiles");
+
+                return true;
+            }
+        });
+
     }
 
+    //Função long press dos botões dos numeros (sem componente de som)
     public void onNumero(View v) {
         Button b = (Button) v;
         TextView tv = (TextView) findViewById(R.id.textoVisor);
         String str = b.getText().toString();
-
         tv.setText(tv.getText() + str);
     }
 
+    //Função long press dos botões dos operadores (sem componente de som)
     public void onOperador(View v) {
         Button b = (Button) v;
         TextView tv = (TextView) findViewById(R.id.textoVisor);
-        if (firstOperation) {
+        if (firstOperation && (tv.getText().toString() != "")) {
             soma = Double.parseDouble(tv.getText().toString());
             tv.setText("");
             lastOperation = b.getText().toString();
@@ -313,48 +345,73 @@ public class MainActivity extends Activity {
         }
     }
 
+    //Função long press do botão igual (sem componente de som)
     public void onEqual(View v) {
         TextView tv = (TextView) findViewById(R.id.textoVisor);
-        switch (lastOperation) {
-            case "+":
-                soma += Double.parseDouble(tv.getText().toString());
-                break;
-            case "-":
-                soma -= Double.parseDouble(tv.getText().toString());
 
-                break;
-            case "X":
-                soma = soma*Double.parseDouble(tv.getText().toString());
+        if (lastOperation != "" && (tv.getText().toString() != "")) {
+            switch (lastOperation) {
+                case "+":
+                    soma += Double.parseDouble(tv.getText().toString());
+                    break;
+                case "-":
+                    soma -= Double.parseDouble(tv.getText().toString());
 
-                break;
-            case "/":
-                soma = soma/Double.parseDouble(tv.getText().toString());
+                    break;
+                case "X":
+                    soma = soma * Double.parseDouble(tv.getText().toString());
+
+                    break;
+                case "/":
+                    soma = soma / Double.parseDouble(tv.getText().toString());
 
 
-                break;
+                    break;
+            }
+
+            Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra("Resultado", soma);
+            startActivity(intent);
+
+            soma = 0;
+            tv.setText("");
+            lastOperation = "";
+            firstOperation = true;
+        } else {
+            //playPling
         }
-
-        Intent intent = new Intent(this,ResultActivity.class);
-        intent.putExtra("Resultado",soma);
-        startActivity(intent);
-
-        soma=0;
-        tv.setText("");
-        lastOperation = "";
-        firstOperation = true;
     }
 
+    //Função long press do botão Apagar Tudo (sem componente de som)
     public void onCE(View v) {
         TextView tv = (TextView) findViewById(R.id.textoVisor);
         tv.setText("");
         soma = 0;
+        lastOperation = "";
         firstOperation = true;
     }
-    
-    public void onC(View v){
+
+    //Função long press do botão Apagar (sem componente de som)
+    public void onC(View v) {
         TextView tv = (TextView) findViewById(R.id.textoVisor);
-        StringBuilder aux = new StringBuilder(tv.getText());
-        aux.deleteCharAt(aux.length() - 1);
-        tv.setText(aux.toString());
+
+        if (tv.getText().toString() != "") {
+            StringBuilder aux = new StringBuilder(tv.getText());
+            aux.deleteCharAt(aux.length() - 1);
+            tv.setText(aux.toString());
+        } else {
+            //playPling
+        }
     }
+
+    //Função long press do botão Tutorial (sem componente de som)
+    public static class onTut extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Selecionou a opção tutorial: As teclas com números estão dispostos na parte esquerda do ecrã e as operações na parte direita. Os números pares são os botões mais escuros com números brancos e os números ímpares são os botões mais claros com números pretos. As operações que pode realizar são as seguintes por esta ordem, de cima para baixo: Soma, Subtração, Multiplicação e Divisão. Quando pretender finalizar a operação carregue no botão igual no canto inferior direito, que vai abrir uma janela a mostrar o resultado do seu cálculo. Os números e sinais que escrever antes de pressionar o botão “=”  vão aparecer no display no canto superior direito da aplicação");
+            return builder.create();
+        }
+    }
+
 }
--- app/src/main/java/com/gps1718/g24/faladora/ResultActivity.java
+++ app/src/main/java/com/gps1718/g24/faladora/ResultActivity.java
@@ -3,7 +3,8 @@ package com.gps1718.g24.faladora;
         import android.app.Activity;
         import android.os.Bundle;
         import android.view.View;
-        import android.widget.Button;
+        import android.view.Window;
+        import android.view.WindowManager;
         import android.widget.TextView;
 
 public class ResultActivity extends Activity {
@@ -11,6 +12,8 @@ public class ResultActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_result);
 
         TextView tv = (TextView) findViewById(R.id.textView2);
